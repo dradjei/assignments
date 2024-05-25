6e13ad6e2965370bfe5fbff0c908c2132b8db538
Derrick <derrick.adjei@csuglobal.edu>
2024-05-24 21:09:54 -0400

    add $

--- Module 4/item.py
+++ Module 4/item.py
@@ -9,7 +9,7 @@ class ItemToPurchase:
         self.item_quantity = 0
     
     def print_item_cost(self):
-        print(self.item_name, self.item_quantity, "@", "$"+str(self.item_price), "=", self.item_price*self.item_quantity)
+        print(self.item_name, self.item_quantity, "@", "$"+str(self.item_price), "=", "$"+str(self.item_price*self.item_quantity))
         
         
 if __name__ == "__main__":
