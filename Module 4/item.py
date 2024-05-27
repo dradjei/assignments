17dc31959b8011bcd0bb0031a3796cc751ab07d0
Derrick <derrick.adjei@csuglobal.edu>
2024-05-26 21:06:37 -0400

    touch up and Module 6

--- Module 4/item.py
+++ Module 4/item.py
@@ -5,7 +5,7 @@ class ItemToPurchase:
     
     def __init__(self):
         self.item_name = "none"
-        self.item_price = 0
+        self.item_price = 0.0
         self.item_quantity = 0
     
     def print_item_cost(self):
